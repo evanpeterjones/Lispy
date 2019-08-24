bc11fcf708e96322983ab5309b569e08d77c4904
evanpeterjones <evanpeterjones@gmail.com>
2019-08-24 08:20:52 -0400

    updates

--- rss.lisp
+++ rss.lisp
@@ -1,15 +1,17 @@
-(ql:quickload '(:dexador :plump :lquery :cl-ppcre) :silent t)
+(ql:quickload '(:dexador :plump :lquery :cl-ppcre :trivial-download) :silent t)
 
 (defparameter podcasts '("http://tinymeatgang.libsyn.com/rssfeed"
                          "http://rss.art19.com/startalk-radio"
                          "http://wakingup.libsyn.com/rss"))
 
 (defun shorten (title-name)
+  "this is horrendous"
   (cl-ppcre:regex-replace-all #\space
   (cl-ppcre:regex-replace-all "-"
   (cl-ppcre:regex-replace-all "_" title-name "") "") ""))
 
 (defun parse-dom (dom-root)
+  "return the list of items"
   (let ((titles (coerce (lquery:$ dom-root "title" (text)) 'list))
         (urls (coerce (lquery:$ dom-root "enclosure" (attr "url")) 'list)))
     (loop for title in titles
@@ -17,16 +19,15 @@
           when (and title url)
           collect (list (shorten title) url))))
 
-(defun download (episode)
-  (let ((file-name ))
-    (With-Open-File (stream file-name :direction :output :if-exists nil)
-                    (format stream "~S" (dex:get (second episode))))))
+(defun path-maker (file-name file-type)
+  (make-pathname
+   :directory '(:absolute "home" "evanpeterjones" "Music" "podcasts")
+   :name file-name
+   :type file-type))
 
 (loop for pod in podcasts 
       do (let ((episodes (parse-dom (plump:parse (dex:get pod)))))
-           (loop for which in '(first second third)
-		do (let ((url (eval `(,which pod))))
-                      (format t "Downloading episode: ~S" url)
-                      (trivial-download:download url (concatenate 'string (first episode) ".mp3"))))))
-
-
+           (loop for which in '(third fourth fifth)
+		do (let ((episode (eval `(,which episodes))))
+                      (format t "Downloading episode: ~S" (first episode))
+                      (trivial-download:download (second episode) (path-maker (first episode) "mp3"))))))
