3b0c8cdaefa1f9fbdc6c49f1623b72f3a04fe0b0
evanpeterjones <evanpeterjones@gmail.com>
2019-09-01 11:12:02 -0400

    amazing'

--- rss.lisp
+++ rss.lisp
@@ -1,8 +1,12 @@
-(ql:quickload '(:dexador :plump :lquery :cl-ppcre) :silent t)
+#!/usr/bin/sbcl --script
 
-(defparameter podcasts '("http://tinymeatgang.libsyn.com/rssfeed"
-                         "http://rss.art19.com/startalk-radio"
-                         "http://wakingup.libsyn.com/rss"))
+(ql:quickload '(:dexador :plump :lquery :cl-ppcre :trivial-download) :silent t)
+
+(defparameter podcasts '(("tiny-meat-gang" "http://tinymeatgang.libsyn.com/rssfeed")
+                         ("startalk" "http://rss.art19.com/startalk-radio")
+			 ("waking-up" "http://wakingup.libsyn.com/rss")))
+
+(defparameter pod-dir "~/Music/podcasts/")
 
 (defun shorten (title-name)
   (cl-ppcre:regex-replace-all #\space
@@ -10,23 +14,34 @@
   (cl-ppcre:regex-replace-all "_" title-name "") "") ""))
 
 (defun parse-dom (dom-root)
-  (let ((titles (coerce (lquery:$ dom-root "title" (text)) 'list))
-        (urls (coerce (lquery:$ dom-root "enclosure" (attr "url")) 'list)))
+  (let ((titles (coerce (lquery:$ dom-root "item" "title" (text)) 'list))
+        (urls (coerce (lquery:$ dom-root "item" "enclosure" (attr "url")) 'list)))
     (loop for title in titles
           for url in urls
           when (and title url)
           collect (list (shorten title) url))))
 
-(defun download (episode)
-  (let ((file-name ))
-    (With-Open-File (stream file-name :direction :output :if-exists nil)
-                    (format stream "~S" (dex:get (second episode))))))
+(defun down (episode podcast-name)
+  "wrapper for the trivial-download:download function, return nil if already downloaded"
+  (let ((file-name (concatenate 'string pod-dir podcast-name "/" (first episode) ".mp3")))
+    (if (probe-file file-name)
+	(progn
+	  (format t "~S already downloaded, skipping~%" file-name)
+	  nil)
+	(progn
+	  (format t "Downloading ~S~%" file-name)
+	  (trivial-download:download (second episode) (concatenate 'string pod-dir podcast-name "/" (first episode) ".mp3"))))))
 
-(loop for pod in podcasts 
-      do (let ((episodes (parse-dom (plump:parse (dex:get pod)))))
-           (loop for which in '(first second third)
-		do (let ((url (eval `(,which pod))))
-                      (format t "Downloading episode: ~S" url)
-                      (trivial-download:download url (concatenate 'string (first episode) ".mp3"))))))
+(defun download (episode-list podcast-name)
+  "recursive function to download episodes until (download %) returns null"
+  (if (down (first episode-list) podcast-name)
+      (download (rest episode-list) podcast-name)))
 
+(defun run ()
+  (loop for pod in podcasts 
+     do (let* ((podcast-name (first pod))
+	       (rss-feed (second pod))
+	       (episodes (parse-dom (plump:parse (dex:get rss-feed)))))
+	  (download episodes podcast-name))))
 
+(run)
